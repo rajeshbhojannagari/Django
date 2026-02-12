78b34b019eefb6f135bef15e1528c39d2c375cab
rajeshbhojannagari <rajeshbhojannagari@gmail.com>
2026-02-12 11:48:54 +0530

    MORE CODE

---
+++ myproject/app/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+def  home(request):
+    return render(request,'viewhome.html')
+# Create your views here.
--- myproject/myproject/views.py
+++ myproject/myproject/views.py
@@ -2,3 +2,5 @@ from django.http import HttpResponse
 from django.shortcuts import render
 def home(request):
     return render(request,'index.html')
+def about(request):
+    return HttpResponse('Hello')
